8ab09ac8d16f208aefd295dd1cf376f5352bbc80
hyuniverse <hyuniverse12@gmail.com>
2025-04-18 18:11:04 +0900

    feat: define Agent JPA repository interfaces
    - AgentRepository for AgentEntity
    - AgentSnapshotRepository for AgentSnapshotEntity
    - JPA 기반 기본 CRUD 지원
    
    Closes #3

---
+++ domain/src/main/java/com/infobank/multiagentplatform/domain/agent/repository/AgentRepository.java
@@ -0,0 +1,7 @@
+package com.infobank.multiagentplatform.domain.agent.repository;
+
+import com.infobank.multiagentplatform.domain.agent.entity.AgentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgentRepository extends JpaRepository<AgentEntity, String> {
+}
