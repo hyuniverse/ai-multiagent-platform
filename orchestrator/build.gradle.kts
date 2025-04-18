ea93bc8d50c8a4975e9afce98dbff0217f8d84db
hyuniverse <hyuniverse12@gmail.com>
2025-04-18 16:19:05 +0900

    chore: broker, orchestrator에 Swagger 및 config 모듈 연동 (#1)

--- orchestrator/build.gradle.kts
+++ orchestrator/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
 }
 
 group = "com.infobank"
@@ -9,6 +10,11 @@ repositories {
 }
 
 dependencies {
+    implementation(project(":config"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
@@ -16,3 +22,4 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
