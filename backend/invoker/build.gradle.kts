293beac7d95f3e7386b087d4eef66ecb19083287
hyuniverse <hyuniverse12@gmail.com>
2025-04-19 17:35:49 +0900

    build: Invoker 모듈 라이브러리 의존성 설정(#7)

--- backend/invoker/build.gradle.kts
+++ backend/invoker/build.gradle.kts
@@ -2,13 +2,23 @@ plugins {
     id("java")
 }
 
-group = "com.infobank"
-
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation(project(":domain"))
+    implementation(project(":common"))
+    testImplementation(project(":domain"))
+    testImplementation(project(":common"))
+
+    // Lombok 추가
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+
+    implementation("org.springframework:spring-web:6.1.4")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
