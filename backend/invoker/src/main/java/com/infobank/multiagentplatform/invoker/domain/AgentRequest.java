86a9bfb0935149ec024a5ee10257d05a28dc109c
hyuniverse <hyuniverse12@gmail.com>
2025-04-19 17:34:27 +0900

    feat: Invoker Domain 구현(#7)

---
+++ backend/invoker/src/main/java/com/infobank/multiagentplatform/invoker/domain/AgentRequest.java
@@ -0,0 +1,16 @@
+package com.infobank.multiagentplatform.invoker.domain;
+
+import lombok.Getter;
+
+@Getter
+public class AgentRequest {
+    private String userInput;
+    private Object input;
+
+    public AgentRequest() {}
+
+    public AgentRequest(String userInput, Object input) {
+        this.userInput = userInput;
+        this.input = input;
+    }
+}
